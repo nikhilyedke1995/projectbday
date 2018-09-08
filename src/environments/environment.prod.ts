a6c0b0dae96664f5f01982bffd7260512a299e0e
Nikhil Yedke <nikhilyedke1995@gmail.com>
2018-09-08 15:55:07 +0530

    Changed prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDJc2R92qn6uAzpO04rDVOEYcFas276aGo",
+    authDomain: "first-angular-e7f32.firebaseapp.com",
+    databaseURL: "https://first-angular-e7f32.firebaseio.com",
+    projectId: "first-angular-e7f32",
+    storageBucket: "first-angular-e7f32.appspot.com",
+    messagingSenderId: "531606280951"
+  }
 };
